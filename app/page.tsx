2afe4fd93496040b3a03575a9f6439bd68223df2
Kuldeep <kunalrajput67718@gmail.com>
2026-04-09 15:29:46 +0530

    database

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import { Button } from "@/components/ui/button";
+import React from "react";
 
 const page = () => {
-  return (
-    <div>
-      <Button>Click me</Button>
-    </div>
-  );
+  return <div>page</div>;
 };
 
 export default page;
