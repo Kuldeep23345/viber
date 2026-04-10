61beaa517b9bf658831ef3876ac1f77964a7214f
Kuldeep <kunalrajput67718@gmail.com>
2026-04-10 14:17:12 +0530

    feat: integrate E2B sandbox environment for Next.js code execution in Inngest functions

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,18 +1,32 @@
 import { inngest } from "./client";
 import { gemini, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
+import { step } from "inngest";
+import { getSandbox } from "./utils";
 export const helloWorld = inngest.createFunction(
   { id: "hello-world", triggers: { event: "app/hello.world" } },
   async ({ event }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("viber-nextjs-test-2");
+      return sandbox.sandboxId;
+    });
+
     const codeAgent = createAgent({
       name: "code-agent",
-      system: "You are and expert next.j developer. You write readble, maintainable code. You write simpel Next.js & React snippets.",
+      system:
+        "You are and expert next.j developer. You write readble, maintainable code. You write simpel Next.js & React snippets.",
       model: gemini({ model: "gemini-2.5-flash" }),
     });
     const { output } = await codeAgent.run(
       `Write the followin snippet ${event.data.value}`,
     );
-    console.log(output);
 
-    return { output};
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   },
 );
